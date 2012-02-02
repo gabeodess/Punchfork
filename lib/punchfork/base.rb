17ff49ce561e1dc8d0fc2f2e04c6c98bfd8ad64e
Gabe Odess <gabeodess@gmail.com>
2012-02-02 12:53:29 -0500

    removed logging

--- lib/punchfork/base.rb
+++ lib/punchfork/base.rb
@@ -4,7 +4,6 @@ module Punchfork
     def search(*args)
       options = args.extract_options!
       search_uri.query = ["key=#{api_key}", "q=#{URI.escape(args.join(','))}", *options.map{ |k,v| "#{k}=#{v}" }].join('&')
-      p ["punchfork search", search_uri.to_s] if Rails.env.test?
       JSON.load(Net::HTTP.get_response(search_uri).body)
     end
       
